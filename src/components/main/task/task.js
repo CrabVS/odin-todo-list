0035d7e6b9a46f6a9270f1a8cd63f4dd673b6f55
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-28 03:59:28 +0200

    Delete star svgs and add icons to content func

--- src/components/main/task/task.js
+++ src/components/main/task/task.js
@@ -1,7 +1,19 @@
 import './task.css';
+import deleteIcon from '../../../res/delete.svg';
+import checkIcon from '../../../res/check-circle.svg';
+import editIcon from '../../../res/note-edit.svg';
 
 const createListeners = function createTaskListeners() {
 
+}
+
+const createIconButtons = function createTaskIconButtons(taskEl) {
+    const iconEl = taskEl.querySelector('.task-buttons');
+
+    iconEl.innerHTML = 
+    `<img src="${deleteIcon}" alt="delete-icon" class="icon">
+    <img src="${checkIcon}" alt="check-icon" class="icon">
+    <img src="${editIcon}" alt="edit-icon" class="icon">`;
 }
 
 const createContent = function createTaskContent(taskEl, taskInfo) {
@@ -15,6 +27,9 @@ const createContent = function createTaskContent(taskEl, taskInfo) {
     headerEls[0].textContent = taskInfo.title;
     headerEls[1].textContent = taskInfo.notes;
     headerEls[2].textContent = taskInfo.duedate;
+
+    createIconButtons(taskEl);
+
 }
 
 const createTask = function createTask(taskInfo) {
